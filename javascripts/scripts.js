8e51f90e207c94bc4be33e36503e45594e7b2278
VIDIT SAXENA <ViditSaxena@VIDITs-MacBook-Pro.local>
2015-08-09 14:02:13 -0400

    writes notes on scripts file

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,27 +1,67 @@
 console.log("loaded")
+//------------------------------------
+//Starting everything below
+
+TicTacToeGame.prototype.readyToStart = function readyToStart(){
+this.player1Name = prompt("Player 1 - Please input your name.")
+$('#player1').text("Player 1 is " + this.player1Name);
+this.player2Name = prompt("Player 2 - Please input your name.")
+$('#player2').text("Player 2 is " + this.player2Name);
+};
+
+// --------------------------
+// TicTacToeGame    public interface....
+function TicTacToeGame(){
+  this.player1Name;
+  this.player2Name;
+  this.moveNumber = 1;
+}
+
+TicTacToeGame.prototype.init = function init(domNode){
+  this.board = this.generatePlayGround();
+  domNode.append(this.board);
+  this.bindClickLetter();
+};
+
+TicTacToeGame.prototype.start = function start(){
+  this.readyToStart();
+};
+
+
+// -------------------------------------
+
+// Start the game
+var game;
+
+$(document).ready(function(){
+  game = new TicTacToeGame();
+
+  game.init( $('#board'));
+  game.start();
+
+});
+
+
 //--------------------------------------
-//Logic Starts now
+//Logic Logic Logic Logic Logic Logic Logic
+
 
 var board =
     [[0,0,0],
      [0,0,0],
      [0,0,0]];
+     //TODO: To store the values when clicked in a box.
 
 var player1 = 'X';
 var player2 = 'O';
+//TODO: Making it static but could have asked dynamically what letters or in this case what color they want to be.
 var turn = player1;
+//TODO: Making player 1 go first.
 var moveNumber = 1;
+//TODO:: Just storing what move number it is.
 var winner = false;
 
-TicTacToeGame.prototype.checkWinner = function checkWinner(){
-
-  TicTacToeGame.prototype.winningRows();
-  TicTacToeGame.prototype.winningCols();
-  TicTacToeGame.prototype.winningDiagonals();
-  TicTacToeGame.prototype.checkTie();
-
-};
-
+//-------------------
 
 TicTacToeGame.prototype.winningRows = function winningRows(){
 
@@ -66,6 +106,7 @@ TicTacToeGame.prototype.winningDiagonals = function winningDiagonals(){
   }
 
 };
+//TODO: Check if the winner wins diagonally
 
 TicTacToeGame.prototype.checkTie = function checkTie(){
 var filledCells = [];
@@ -81,34 +122,43 @@ for (var i = 0; i < 3; i++) {
   }
 };
 
+//TODO: Check if the game ends in a tie
+
+TicTacToeGame.prototype.checkWinner = function checkWinner(){
+
+  TicTacToeGame.prototype.winningRows();
+  TicTacToeGame.prototype.winningCols();
+  TicTacToeGame.prototype.winningDiagonals();
+  TicTacToeGame.prototype.checkTie();
+
+};
+
+//TODO: Check all the winning combos
+
 
 // End the game, and refresh the page
 TicTacToeGame.prototype.endGame = function endGame(message) {
 
   	alert(message);
-  	location.reload();
-}
+    var reload = confirm("Wanna play again?");
 
+    if (reload == true) {
+    location.reload();
+     } else {
+     alert("Thank You for playing!");
+}
+};
+
+//TODO: Reloading the game
+
+//---------------------------------------------------------------------
+//Generating the HTML below through jquery
 
 TicTacToeGame.prototype.generateBoxNode = function generateBoxNode(){
   var boxNode = $('<td>').addClass('cell');
   return boxNode;
 }
 // TODO: Generate Box node
-
-
-TicTacToeGame.prototype.storeCell = function storeCell(){
-
-
-$('.cell').on('click', function(e) {
-  var filledCell = $(e.target);
-
-  var row = filledCell.data('row');
-  var col = filledCell.data('col');
-  board[row][col] = turn;
-
-})
-};
 
 
 
@@ -150,41 +200,17 @@ TicTacToeGame.prototype.bindClickLetter = function bindClickLetter(){
 
 // TODO: Generates a letter when the user clicks on the board
 
-TicTacToeGame.prototype.readyToStart = function readyToStart(){
-this.player1Name = prompt("Player 1 - Please input your name.")
-$('#player1').text("Player 1 is " + this.player1Name);
-this.player2Name = prompt("Player 2 - Please input your name.")
-$('#player2').text("Player 2 is " + this.player2Name);
+
+TicTacToeGame.prototype.storeCell = function storeCell(){
+
+
+$('.cell').on('click', function(e) {
+  var filledCell = $(e.target);
+
+  var row = filledCell.data('row');
+  var col = filledCell.data('col');
+  board[row][col] = turn;
+
+})
 };
-
-// --------------------------
-// TicTacToeGame    public interface....
-function TicTacToeGame(){
-  this.player1Name;
-  this.player2Name;
-  this.moveNumber = 1;
-}
-
-TicTacToeGame.prototype.init = function init(domNode){
-  this.board = this.generatePlayGround();
-  domNode.append(this.board);
-  this.bindClickLetter();
-};
-
-TicTacToeGame.prototype.start = function start(){
-  this.readyToStart();
-};
-
-
-// -------------------------------------
-
-// Start the game
-var game;
-
-$(document).ready(function(){
-  game = new TicTacToeGame();
-
-  game.init( $('#board'));
-  game.start();
-
-});
+// TODO:extracting the value when a cell is clicked and storing it in the board array
